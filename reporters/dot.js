171ddf92d5bda3bd2bd145a5a50b5a47ff92e9dd
Alexander Olsson <alexo@axis.com>
2015-06-25 15:36:21 +0200

    Added xunit reporter
    Change-Id: Id260910cf338aac43540150b7c7ab79c6967a761

--- reporters/dot.js
+++ reporters/dot.js
@@ -14,7 +14,7 @@ function repeat(str, n) {
   return new Array(n + 1).join(str);
 }
 
-var r = {
+var m = {
   results: [],
   totalTasks: 0,
   column: 0,
@@ -22,71 +22,71 @@ var r = {
   padding: '    ',
 
   init: function (totalTasks, workerLength) {
-    r.totalTasks = totalTasks;
-    r.maxColumns = Math.min(
-      (process.stdout.columns || 80) - r.padding.length - 2 * ('' + totalTasks).length - 20, 80
+    m.totalTasks = totalTasks;
+    m.maxColumns = Math.min(
+      (process.stdout.columns || 80) - m.padding.length - 2 * ('' + totalTasks).length - 20, 80
     );
 
-    process.stdout.write(fmt('\n%sRunning %d tests with %d parallel jobs\n', r.padding, totalTasks, workerLength));
-    process.stdout.write(r.padding); // Initial padding. Padding for other rows are added in `printProgress`
+    process.stdout.write(fmt('\n%sRunning %d tests with %d parallel jobs\n', m.padding, totalTasks, workerLength));
+    process.stdout.write(m.padding); // Initial padding. Padding for other rows are added in `printProgress`
   },
 
   progress: function (result) {
-    r.results.push(result);
+    m.results.push(result);
     [ 'passes', 'pending', 'failures' ].forEach(function (resultType) {
-      var n = Math.min(r.maxColumns - r.column, result.stats[resultType]);
+      var n = Math.min(m.maxColumns - m.column, result.stats[resultType]);
       var remaining = result.stats[resultType] - n;
 
-      r.printDots(resultColors[resultType], n);
+      m.printDots(resultColors[resultType], n);
 
-      if (r.column === r.maxColumns ) {
-        r.printProgress();
-        r.column = 0;
+      if (m.column === m.maxColumns ) {
+        m.printProgress();
+        m.column = 0;
       }
 
-      r.printDots(resultColors[resultType], remaining);
+      m.printDots(resultColors[resultType], remaining);
     });
 
   },
 
   epilogue: function (resultSet) {
-    process.stdout.write(repeat(' ', r.maxColumns - r.column));
-    r.printProgress();
+    process.stdout.write(repeat(' ', m.maxColumns - m.column));
+    m.printProgress();
 
-    r.results.forEach(function (result) {
+    m.results.forEach(function (result) {
       result.failures.forEach(function (failure) {
         process.stdout.write('\n\n');
-        process.stdout.write(r.padding + ('Failure: ' + failure.fullTitle).red + '\n');
-        process.stdout.write(r.padding + failure.err.message + '\n\n');
-        process.stdout.write(r.padding + failure.err.stack.replace(/\n/g, '\n' + r.padding, 1) + '\n');
+        process.stdout.write(m.padding + 'Failure: ' + failure.fullTitle + '\n');
+        process.stdout.write(m.padding + failure.err.message.red + '\n');
+        process.stdout.write(m.padding + failure.err.stack.replace(/\n/g, '\n' + m.padding, 1) + '\n');
       });
     });
 
     process.stdout.write('\n');
     var passing = 0, failures = 0, pending = 0;
-    r.results.forEach(function (result) {
+    m.results.forEach(function (result) {
       passing += result.stats.passes;
       failures += result.stats.failures;
       pending += result.stats.pending;
     });
 
-    process.stdout.write(r.padding + (passing + ' passing')[resultColors['passes']] + '\n');
-    process.stdout.write(r.padding + (pending + ' pending')[resultColors['pending']] + '\n');
-    process.stdout.write(r.padding + (failures + ' failing')[resultColors['failures']] + '\n');
+    process.stdout.write(m.padding + (passing + ' passing')[resultColors['passes']] + '\n');
+    process.stdout.write(m.padding + (pending + ' pending')[resultColors['pending']] + '\n');
+    process.stdout.write(m.padding + (failures + ' failing')[resultColors['failures']] + '\n');
   },
 
   printDots: function (color, n) {
-    r.column += n;
+    m.column += n;
     process.stdout.write(repeat('.'[color], n));
   },
 
   printProgress: function () {
-    process.stdout.write(fmt(' Tests: %d/%d (%d %)\n' + r.padding,
-      r.totalTasks - parallelizer.queue.length,
-      r.totalTasks,
-      Math.floor(100 * (r.totalTasks - parallelizer.queue.length) / r.totalTasks)));
+    process.stdout.write(fmt(' Tests: %d/%d (%d %)\n' + m.padding,
+      m.totalTasks - parallelizer.queue.length,
+      m.totalTasks,
+      Math.floor(100 * (m.totalTasks - parallelizer.queue.length) / m.totalTasks)));
   }
 
 };
 
-module.exports = r;
+module.exports = m;
--- reporters/xunit.js
+++ reporters/xunit.js
@@ -1,14 +1,45 @@
 'use strict';
+var libxmljs = require('libxmljs');
 
-var x = {
+var m = {
+  tests: [],
+  starttime: null,
+
   init: function () {
+    m.startTime = new Date();
   },
 
-  progress: function () {
+  progress: function (result) {
+    Array.prototype.push.apply(m.tests, result.tests);
   },
 
   epilogue: function () {
+    var endTime = new Date();
+    var doc = libxmljs.Document();
+    var testsuite = doc.node('testsuite').attr({
+      name: 'Mocha tests',
+      tests: 0,
+      failures: 0,
+      errors: 0,
+      skipped: 0,
+      timestamp: endTime.toUTCString(),
+      time: (endTime - m.startTime) / 1000
+    });
+
+    m.tests.forEach(function (test) {
+      var testcase = testsuite.node('testcase').attr({
+        classname: test.fullTitle,
+        name: test.title,
+        time: (test.duration || 0) / 1000
+      });
+
+      if (test.err.stack) {
+        testcase.node('failure').cdata(test.err.stack);
+      }
+    });
+
+    process.stdout.write(doc.toString());
   }
 };
 
-module.exports = x;
+module.exports = m;
