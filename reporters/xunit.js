b0d3fef78d3706cffac7aa6da6dd2d3d8b39868e
Alexander Olsson <alexo@axis.com>
2015-06-25 16:28:31 +0200

    xunit reporter: include global data in <testsuite>
    Change-Id: Idd62ef8164019bbeea1138bf3d2126db7e9a3826

--- reporters/xunit.js
+++ reporters/xunit.js
@@ -1,27 +1,41 @@
 'use strict';
 var libxmljs = require('libxmljs');
+var parallelizer = require('../lib/parallelizer');
+var fmt = require('util').format;
 
 var m = {
   tests: [],
+  passes: [],
+  pending: [],
+  failures: [],
   starttime: null,
+  totalTasks: 0,
 
-  init: function () {
+  init: function (totalTasks) {
     m.startTime = new Date();
+    m.totalTasks = totalTasks;
+    process.stderr.write('\n');
   },
 
   progress: function (result) {
     Array.prototype.push.apply(m.tests, result.tests);
+    Array.prototype.push.apply(m.passes, result.passes);
+    Array.prototype.push.apply(m.pending, result.pending);
+    Array.prototype.push.apply(m.failures, result.failures);
+
+    process.stderr.write(fmt('\r%d (out of %d) task(s) not yet started (%d success, %d failures, %d pending)',
+      parallelizer.queue.length, m.totalTasks, m.passes.length, m.failures.length, m.pending.length));
   },
 
   epilogue: function () {
     var endTime = new Date();
     var doc = libxmljs.Document();
     var testsuite = doc.node('testsuite').attr({
-      name: 'Mocha tests',
-      tests: 0,
-      failures: 0,
-      errors: 0,
-      skipped: 0,
+      name: 'Mopar mocha tests',
+      tests: m.tests.length,
+      failures: m.failures.length,
+      errors: m.failures.length,
+      skipped: m.pending.length,
       timestamp: endTime.toUTCString(),
       time: (endTime - m.startTime) / 1000
     });
